5dbe37bb5d0567e4e8bb95e8106874da6104366f
mario <kinuthiamariodavis@gmail.com>
2023-10-03 15:44:26 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+newHeader.textContent = 'Brian Cherus is the champion';
+
+document.body.appendChild(newHeader);
+
+const mainElement = document.querySelector('#main');
+if (mainElement) {
+  mainElement.remove();
+}
